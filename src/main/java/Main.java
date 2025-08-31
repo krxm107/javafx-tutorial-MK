794020c817cb9b761112cb8931bca81fa9c6d62b
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-31 15:09:58 +0800

    Do Part of Level 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,8 +34,6 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -74,5 +72,20 @@ public class Main extends Application {
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
         //More code to be added here later
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+    }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        userInput.clear();
     }
 }
